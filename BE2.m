12fe1af2aad4e436e21c090b34b829c6f97cc713
Eyskha <66302196+Eyskha@users.noreply.github.com>
2021-03-08 11:40:18 +0100

    Commentaires 210-... et vertical limits

--- BE2.m
+++ BE2.m
@@ -44,7 +44,7 @@ end
 % thresholdSelection : si dans les pixels les plus clairs (seuil = % de 
 %    pixels) alors mis à 255 dans image en niveaux de gris
 
-% thresholdBinary : si sup au seuil alors 1 sinon 0 pour image binaire
+% thresholdBinary : si intensité supérieure au seuil alors 1 sinon 0 pour image binaire
 
 % thresholdTextRegionDistance : si distance des 2 plus hauts pics de 
 %    l'histogramme est inférieure au seuil alors supprimée des regions de
@@ -58,8 +58,8 @@ end
 function f=detection_texte(image,type,intermediateDisplay,thresholdSelection,thresholdBinary,resizeFactor,thresholdTextRegionDistance,thresholdM4)
     %% 3.1 Digital image tranformation
     % Conversion jpg to bmp
-%     I = imread(strcat('Images\',image,type));
-%     imwrite(I,strcat('Images\',image,'.bmp'));
+	% I = imread(strcat('Images\',image,type));
+    % imwrite(I,strcat('Images\',image,'.bmp'));
 
     % Get RGB image
     I = image;
@@ -121,7 +121,6 @@ function f=detection_texte(image,type,intermediateDisplay,thresholdSelection,thr
         % function Gsortie = bckgrndPixelSeparation(G)
 
     % Effective text region filtering
-    % - Calcul distance entre maxs : si sup à un seuil (15po) alors calssifié comme region de texte
 
         % Get regions limits for low resolution
     regionsLimitsRaw = regionprops(ITextRegion); %on récupère les propriétés des différentes régions de l'image (les rectangles)
@@ -135,11 +134,11 @@ function f=detection_texte(image,type,intermediateDisplay,thresholdSelection,thr
     
     rBis = []; % Ajust limits to enter image size
     for i=1:height(regionsLimitsHighRes) %pour tous les rectangles
-        toplefty = max(regionsLimitsHighRes(i,1),1); %on s'assure que le point en haut à gauche ne sort pas de l'image
-        topleftx = max(regionsLimitsHighRes(i,2),1);
-        widthRect = min(regionsLimitsHighRes(i,3),width(I)-toplefty); %on s'assure que la droite du rectangle ne sort pas de l'image
-        heightRect = min(regionsLimitsHighRes(i,4),height(I)-topleftx); %on s'assure que le bas du rectangle ne sort pas de l'image
-        rBis= [rBis ; [toplefty topleftx widthRect heightRect]]; %on stocke les nouvelles limites obtenues
+        topleftx = max(regionsLimitsHighRes(i,1),1); %on s'assure que le point en haut à gauche ne sort pas de l'image
+        toplefty = max(regionsLimitsHighRes(i,2),1);
+        widthRect = min(regionsLimitsHighRes(i,3),width(I)-topleftx); %on s'assure que la droite du rectangle ne sort pas de l'image
+        heightRect = min(regionsLimitsHighRes(i,4),height(I)-toplefty); %on s'assure que le bas du rectangle ne sort pas de l'image
+        rBis= [rBis ; [topleftx toplefty widthRect heightRect]]; %on stocke les nouvelles limites obtenues
     end
     regionsLimitsHighRes = rBis;
     
@@ -154,12 +153,12 @@ function f=detection_texte(image,type,intermediateDisplay,thresholdSelection,thr
         figure();
     end
     for i=1:height(regionsLimitsHighRes)
-        toplefty = regionsLimitsHighRes(i,1);
-        topleftx = regionsLimitsHighRes(i,2);
+        topleftx = regionsLimitsHighRes(i,1);
+        toplefty = regionsLimitsHighRes(i,2);
         widthRect = regionsLimitsHighRes(i,3);  
         heightRect = regionsLimitsHighRes(i,4);
         % Histogram for each region
-        H = imhist(G(topleftx:topleftx+heightRect,toplefty:toplefty+widthRect));
+        H = imhist(G(toplefty:toplefty+heightRect,topleftx:topleftx+widthRect));
         if intermediateDisplay %plot intermédiaire de l'histogramme
             subplot(3,3,i), plot(H), xlim([0 255]);
         end
@@ -216,32 +215,35 @@ function J = M2(I)
     J = I;
     [m,n] = size(I);
     % Top left and bottom right corners
+    % Parcours des pixels de l'image
     for i=1:m-1
         topleft = 0;
         topright = 0;
         bottomleft = 0;
         bottomright = 0;
         for j=1:n
-           % Get top left corner
+           % Get top left corner : prend comme valeur l'abscisse du premier pixel à 1 de la ligne
            if I(i,j)==1 && topleft == 0 && j~=n
                topleft = j;
            end
-           % Get top right corner
+           % Get top right corner : prend comme valeur l'abscisse du dernier pixel à 1 de la ligne (parcours de la ligne de droite à gauche)
            if I(i,n-j+1)==1 && topright == 0 && j~=n
                topright = n-j+1;
            end
-           % Get bottom right corner
+           % Get bottom right corner : prend comme valeur l'abscisse du premier pixel à 1 de la ligne suivante
            if topleft ~= 0 && I(i+1,j)==1
                bottomright = j;
            end
-           % Get bottom left corner
+           % Get bottom left corner : prend comme valeur l'abscisse du dernier pixel à 1 de la ligne suivante (parcours de la ligne de droite à gauche)
            if topright ~= 0 && I(i+1,n-j+1)==1
                bottomleft = n-j+1;
            end
         end
+        % Si les coins supérieur gauche et inférieur droit sont definis, on met à 1 les pixels appartenant au "rectangle"
         if topleft ~= 0 && bottomright ~= 0
             J(i:i+1,topleft:bottomright) = 1;
         end
+        % Si les coins supérieur droite et inférieur gauche sont definis, on met à 1 les pixels appartenant au "rectangle"
         if topright ~= 0 && bottomleft ~= 0
             J(i:i+1,bottomleft:topright) = 1;
         end
@@ -252,7 +254,8 @@ end
 function J = M3(I)
     J = I;
     [m,n] = size(I);
-    % Top left and bottom right corners
+    % Pour chaque pixel à 1, on regarde si le pixel en bas à gauche ou en bas à droite est à 1
+    % Si oui, alors on met à 1 les deux pixels voisins pour compléter le carré
     for i=1:m-1
         for j=1:n
             if (I(i,j) == 1 && I(i+1,j+1) == 1) || (I(i+1,j) == 1 && I(i,j+1) == 1)
@@ -265,32 +268,36 @@ end
 % 3.4 : Background pixels separation : back to G (gray levels image)
 function Gsortie = bckgrndPixelSeparation(G,intermediateDisplay,thresholdSelection)
     % Contruction intervalle [u,L]
-    Hist = imhist(G);
-    if intermediateDisplay
+    Hist = imhist(G); % calcul de l'histogramme de l'image en niveaux de gris
+    if intermediateDisplay % plot intermediaire
         subplot(2,5,8), plot(Hist), xlim([0 255]), title("Grayscale image histogram");
     end
     
-    nbPixels = Hist(255); u = 255; L = 255;
+    nbPixels = Hist(255); u = 255; L = 255; % initialisation des bornes de l'intervalle
     [m,n] = size(G);
     nbTotPixels = m*n;
 
+    % On récupère la valeur d'un pixel (stockee dans u) telle que la proportion de pixels avec
+    % une valeur supérieure ou égale soit égale au seuil défini (thresholdSelection)
     while nbPixels <= thresholdSelection*nbTotPixels
-        u = u - 1;
-        nbPixels = nbPixels + Hist(u);
+        u = u - 1; % diminution de la limite si nombre de pixel non atteint
+        nbPixels = nbPixels + Hist(u); % ajout des pixels de valeur u
     end
     
-    G = double(G); %sinon pb avec multiplication matrice car int
+    G = double(G); %sinon probleme avec multiplication matrice car int.
+    % Eq2 : conservation des valeurs des pixels de valeur <= à u et mise à 255 des autres
     G2 = uint8((G<=u).*G + (G>u)*L);
-    if intermediateDisplay
+    if intermediateDisplay % plot intermediaire
         subplot(2,5,9), imshow(G2), title("Method 2 pixel separation");
     end
 
     G3 = uint8((G<=u)*u + (G>u)*L);
-    if intermediateDisplay
+    % Eq3 : pixels de valeur <= à u prennent valeur u et mise à 255 des autres
+    if intermediateDisplay % plot intermediaire
         subplot(2,5,10), imshow(G3), title("Method 3 pixel separation");
     end
     
-    Gsortie = G2;
+    Gsortie = G2; % on opte pour l'equation 2
 end
 
 %3.5.1 Improving text region localization
@@ -309,54 +316,81 @@ function newTextRegionsLimits = improvementLocalization(G,textRegions,L)
         max = 0;
         maxCurrentLine = 0;
         indexReference = x;
+        % Parcours de chaque ligne
         for i=x:x+h
             for j=y:y+l
+                % Comptage du nombre de pixel de valeur max sur la ligne
                 if G(i,j)==L
                     maxCurrentLine = maxCurrentLine + 1;
                 end
             end
+            % Si ligne actuelle contient plus de pixel max alors devient la nouvelle référence
             if max < maxCurrentLine
                 max = maxCurrentLine;
                 indexReference = i;
             end
         end
         
-        % Comparison with precedent line to get newX
+        % Comparison with precedent line to get newX : recursivity
         newX = comparisonPrecedentLine(indexReference,y,l,G,L);
         
-        % Comparison with next line to get newH
-        newH = comparisonNextLine(indexReference,y,l,G,L)-newX;
+        % Comparison with next line to get newH : recursivity
+        % Différence avec newX pour avoir la hauteur et non l'indice de la ligne
+        newH = comparisonNextLine(indexReference,y,l,G,L)-newX; 
         
-        % No vertical changes for now
+        % Vertical limits
         newY = y;
-        newL = l;
-        for i=newX:newX+newH
-            leftTest = 1; rightTest = 1;
-            while rightTest && newY + newL < length(G)
-                if G(i,newY + newL + 1)==L
-                    newL = newL + 1;
-                else
-                    rightTest = 0;
+        newL = l; newLcopie = 0;
+        
+        % Boucle tant que la largeur de la region change
+        while(newL ~= newLcopie)
+            newLcopie = newL;
+            
+            % Parcours des lignes de l'image
+            for i=newX:newX+newH
+                leftTest = 1; rightTest = 1; %conditions pour continuer les tests ou non
+                % Tant que la largeur est modifiée et que la limite gauche de l'image n'est pas atteinte
+                while rightTest && newY + newL < length(G)
+                    % Si le pixel à droite de la ligne est de valeur max,
+                    % on augmente la largeur de la region de 1
+                    % Sinon on arrete le test
+                    if G(i,newY + newL + 1)==L
+                        newL = newL + 1;
+                    else
+                        rightTest = 0;
+                    end
                 end
-            end
-            while leftTest && newY > 1
-                if G(i,newY-1)==L
-                    newY = newY - 1;
-                    newL = newL + 1;
-                else
-                    leftTest = 0;
+                
+                % Tant que la largeur est modifiée et que la limite droite de l'image n'est pas atteinte
+                while leftTest && newY > 1
+                    % Si le pixel à gauche de la ligne est de valeur max,
+                    % on augmente la largeur de la region de 1 et on décale
+                    % la limite gauche de 1
+                    % Sinon on arrete le test
+                    if G(i,newY-1)==L
+                        newY = newY - 1;
+                        newL = newL + 1;
+                    else
+                        leftTest = 0;
+                    end
                 end
             end
         end
         
+        % On stocke les nouvelles limites dans la matrice de sortie
         newTextRegionsLimits = [newTextRegionsLimits ; [newY newX newL newH]];
     end 
 end
 
 function x=comparisonPrecedentLine(indexLine,y,l,G,L)
+    % Fonction récursive qui permet de récupérer la limite horizontale
+    % supérieure
+    % Si on atteint la limite supérieure de l'image on la renvoie
     if indexLine == 1
         x = 1;
     else
+        % On récupère les listes des indices des pixels de valeur max de la
+        % ligne de ref et celle au dessus
         posR = []; posPrecR = [];
         for j=y:y+l
             if G(indexLine,j)==L
@@ -367,6 +401,9 @@ function x=comparisonPrecedentLine(indexLine,y,l,G,L)
             end
         end
 
+        % Si l'intersection des listes est non vide alors la ligne du
+        % dessus devient la ligne de ref et on recommence par récursivité
+        % Sinon on conserve la ligne de ref actuelle et on la renvoie
         if length(intersect(posR,posPrecR))>0
             x = comparisonPrecedentLine(indexLine-1,y,l,G,L);
         else
@@ -376,9 +413,14 @@ function x=comparisonPrecedentLine(indexLine,y,l,G,L)
 end
 
 function h=comparisonNextLine(indexLine,y,l,G,L)
+    % Fonction récursive qui permet de récupérer la limite horizontale
+    % inférieure
+    % Si on atteint la limite inférieure de l'image on la renvoie
     if indexLine == height(G)
         h = height(G);
     else
+        % On récupère les listes des indices des pixels de valeur max de la
+        % ligne de ref et celle en dessous
         posR = []; posNextR = [];
         for j=y:y+l
             if G(indexLine,j)==L
@@ -389,6 +431,9 @@ function h=comparisonNextLine(indexLine,y,l,G,L)
             end
         end
 
+        % Si l'intersection des listes est non vide alors la ligne du
+        % dessous devient la ligne de ref et on recommence par récursivité
+        % Sinon on conserve la ligne de ref actuelle et on la renvoie
         if length(intersect(posR,posNextR))>0
             h = comparisonNextLine(indexLine+1,y,l,G,L);
         else
@@ -405,17 +450,19 @@ function J = M4(I,thresholdM4)
         left = 0;
         right = 0;
         j = 1;
-        while (~left || ~right) && j <= n
-           % Get left
+        % Pour chaque ligne, on cherche les 2 pixels à 1 les plus eloignes
+        while (~left || ~right) && j <= n % Arret si 2 pixels trouvés ou si limite de l'image atteinte
+           % Get left : prend valeur de l'abscisse du premier pixel à 1 en partant de la gauche
            if left == 0 && I(i,j)==1
                left = j;
            end
-           % Get right
+           % Get right : prend valeur de l'abscisse du premier pixel à 1 en partant de la droite
            if right == 0 && I(i,n-j+1)==1
                right = n-j+1;
            end
-           j = j+1;
+           j = j+1; % Passage a l'abscisse suivante
         end
+        % Si la distance entre 2 pixels est supérieure au seuil, alors mise à 0 des pixels intermédiaires
         if right-left > thresholdM4*n
             J(i,left+1:right-1) = 0;
         end
@@ -425,10 +472,12 @@ end
 function J = M5(I)
     J = I;
     [m,n] = size(I);
+    % Parcours des pixels de l'image n'appartenant pas aux bords
     for i=2:m-1
         for j=2:n-1
+            % Si 8 voisins sont à 0 et que le pixel vaut 1
             if I(i,j)==1 && isequal(I(i-1,j-1:j+1),[0 0 0]) && isequal(I(i+1,j-1:j+1),[0 0 0]) && I(i,j-1)==0 && I(i,j+1)==0
-                J(i,j) = 0;
+                J(i,j) = 0; % mise à 0 du pixel
             end
         end
     end
